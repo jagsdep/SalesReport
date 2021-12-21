65f4e5f9c2a9b96bb0af7f6c70e7c05a61138355
Jagruti <jagruti.depan@gmail.com>
2021-12-21 11:41:28 -0600

    Commented each line

--- sales_report.py
+++ sales_report.py
@@ -4,27 +4,27 @@ salespeople = []  #Created an empty list for salesperson
 melons_sold = [] #Created an empty list for melons sold
 melons_price = [] #created an empty list for melon's price
 
-f = open('sales-report.txt')
+f = open('sales-report.txt') #opens the existing sales-repport files 
 
-for line in f:
-    line = line.rstrip()
-    entries = line.split('|')
+for line in f: #reading the file line by line 
+    line = line.rstrip() #it removes the white spaces at the end of the string
+    entries = line.split('|') # splits each word in the list in string
 
-    salesperson = entries[0]
-    melons = int(entries[2])
-    price = float(entries[1])
+    salesperson = entries[0] #each word/string of index 0 is saved in salesperson 
+    melons = int(entries[2]) #int converst the string to integers and saves each number of index 2 in melons
+    price = float(entries[1]) #int converst the string to float datatype and saves each number of index1 in price
 
-    if salesperson in salespeople:
-        position = salespeople.index(salesperson)
+    if salesperson in salespeople:#conditions to be checked and if the condition is true
+        position = salespeople.index(salesperson)#assign each salesperson on index 0 from salespepole to position
 
-        melons_sold[position] += melons
-        melons_price[position]+= price
+        melons_sold[position] += melons#the melons sold for that position by that salesperson is then added and assigned to melons_sold
+        melons_price[position]+= price #the price for the  melon types sold for that position by that salesperson is then added and assigned to melons_price
         
-    else:
-        salespeople.append(salesperson)
-        melons_sold.append(melons)
-        melons_price.append(price)
+    else: #if the condition does not match
+        salespeople.append(salesperson)#add salesperson
+        melons_sold.append(melons)#add melons sold
+        melons_price.append(price)#add melon's price
 
-
-for i in range(len(salespeople)):
+# print(range(len(salespeople)))
+for i in range(len(salespeople)):#iterates through grouped salespeople 
     print(f'{salespeople[i]} sold {melons_sold[i]} melons for ${melons_price[i]:.2f}')
